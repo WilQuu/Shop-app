0f2946a6a501c51f24b7d7fb5cfb977fc22e0196
Grzegorz Kuchta <50849704+WilQuu@users.noreply.github.com>
2020-12-21 20:46:08 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, redirect, url_for, render_template, request, session
+
+app = Flask(__name__)
+app.secret_key = "Fake zalando"
+
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route("/login",methods=["POST","GET"])
+def login():
+    if request.method == "POST":
+        user = request.form["nm"]
+        session["user"] = user
+        return redirect(url_for("user"))
+    else:
+        return render_template("login.html")
+
+@app.route("/<usr>")
+def user():
+    if "user" in session:
+        user = session["user"]
+        return f"<h1>{user}</h1>"
+    else:
+        return redirect(url_for("login"))
+
+if __name__ == '__main__':
+    app.run(debug=True)
